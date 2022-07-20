151510df640b921ab95bc631b24313a8e3d3ac2d
Acer <kulna>
2022-07-20 09:01:57 +0600

    init: start home works

---
+++ src/components/newState.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+function Example() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>Вы кликнули {count} раз(а)</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+            <button onClick={() => setCount(count - 1)}>
+                Click me
+            </button>
+        </div>
+    );
+}
+
+export default Example;
