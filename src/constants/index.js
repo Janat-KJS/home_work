8455d5dfb741d71d03cd3bc8d911a2f79672ca10
Acer <kulna>
2022-07-21 14:01:07 +0600

    feat: 21-07 test

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,42 @@
 export const tags = ['Travel', 'New York', 'NORWAY', 'London', 'IKEA', 'DIY', 'Ideas', 'FAMALY',];
+
+
+export const posts = [
+    {
+        img: "https://www.w3schools.com/w3images/workshop.jpg",
+        name: "Lorem",
+        description: "Sed mattis nunc"
+    },
+    {
+        img: "https://www.w3schools.com/w3images/gondol.jpg",
+        name: "Ipsum",
+        description: "Praes tinci sed"
+    },
+    {
+        img: "https://www.w3schools.com/w3images/skies.jpg",
+        name: "Dorum",
+        description: "Ultricies congue"
+    },
+    {
+        img: "https://www.w3schools.com/w3images/rock.jpg",
+        name: "Mingsum",
+        description: "Lorem ipsum dipsum"
+    },
+]
+
+export const blog = [
+    {
+        img: "https://www.w3schools.com/w3images/woods.jpg",
+        title: "TITLE HEADING",
+        date: "April 7, 2014",
+        p: "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+        comment: "0"
+    },
+    {
+        img: "https://www.w3schools.com/w3images/bridge.jpg",
+        title: "BLOG ENTRY",
+        date: "April 2, 2014",
+        p: "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.",
+        comment: "2"
+    }
+]
